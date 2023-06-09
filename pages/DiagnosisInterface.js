f24a1dfa6bb6ff93a2f1f2b1b8c592a8bff8b740
Michelle <tianw0420@gmail.com>
2023-06-08 20:37:06 -0700

    prototype

--- pages/DiagnosisInterface.js
+++ pages/DiagnosisInterface.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 import { useState } from "react";
-import { Button } from "react-native-web";
 
 export default function DiagnosisInterface({ navigation }) {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answer, setAnswer] = useState(null); 
+  const [textInput, setTextInput] = useState("");
   const questions = [
     {
       questionText: "What is the Patient's Sex?",
@@ -22,7 +23,46 @@ export default function DiagnosisInterface({ navigation }) {
       questionText: "What Brings the Patient Here?",
       answerOptions: ["Diarrhea", "Diarrhea with Fever"],
     },
+    {
+      questionText: "Does the Patient Have a Fever?",
+      answerOptions: ["Yes", "No", "Don't Know"],
+    }, 
+    {
+      questionText: "Is there Blood in the Diarrhea?",
+      answerOptions: ["Yes", "No", "Don't Know"],
+    }, 
+    { 
+      questionText: "How many Days has Diarrhea Lasted?",
+      answerOptions: ["Don't Know"],
+    }, 
+    {
+      questionText: "Is the Patient Dehydrated?",
+      answerOptions: ["Yes", "No", "Don't Know"],
+    }, 
+    {
+      questionText: "What is the Patient's Temperature?",
+      answerOptions: ["No Thermometer, Feels Okay", "No Thermometer, Feels Feverish"],
+    }, 
   ];
+  const onText = (e) => {
+    setTextInput(e);
+    setAnswer(e);
+  }
+  const onSubmitQuestion = (e) => {
+    console.log(e); 
+    setAnswer(null); 
+    setTextInput("");
+    if (currentQuestion < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      Diagnose(); 
+    }
+
+  }
+  const Diagnose = () => {
+    //diagnose the patient and give treatment options
+  }
+
   return (
     <View style={styles.container}>
       <Text>Quiz </Text>
@@ -31,12 +71,13 @@ export default function DiagnosisInterface({ navigation }) {
         {questions[currentQuestion].answerOptions.map((text) => {
           return (
             <View>
-              <Text>{text}</Text>
+              <Button title={text} onPress={() => setAnswer(text)} />
             </View>
           );
         })}
-      </Text>
-
+      </Text> 
+      <TextInput placeholder={"other..."} value={textInput} onChangeText={onText}/>
+      <Button title="Next" onPress={onSubmitQuestion}/> 
       <StatusBar style="auto" />
     </View>
   );
